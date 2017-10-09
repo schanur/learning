26ad01d0f64a1e1954d59f3efdc9c5c077b581da
Bjoern Griebenow <b.griebenow@web.de>
2017-10-09 09:57:34 +0200

    Add first Rust lesson "gessing_game"

---
+++ rust/the_book_2_examples/guessing_game/src/main.rs
@@ -0,0 +1,46 @@
+#![warn(bad_style,
+        unused, unused_extern_crates, unused_import_braces,
+        unused_qualifications, unused_results)]
+
+// #[cfg(all(feature="winit", feature="glium"))] #[macro_use] extern crate conrod;
+// #[cfg(all(feature="winit", feature="glium"))] mod support;
+
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    // println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        // let guess: u32 = guess.trim().parse()
+        //     .expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
